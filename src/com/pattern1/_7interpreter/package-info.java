51e8dc8a67b012c98ea316652d5da39da520c728
xudj <it_xdj@126.com>
2016-09-18 22:49:48 +0800

    添加解释器模式v1.0

---
+++ src/com/pattern1/_7interpreter/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * @descrition 解释器模式
+ * @author xudj
+ * @date 2016年9月18日 下午10:38:34
+ * @version 1.0
+ */
+/**
+ * 解释器模式（Interpreter Pattern）提供了评估语言的语法或表达式的方式，它属于行为型模式。这种模式实现了一个表达式接口，该接口解释一个特定的
+ * 上下文。这种模式被用在SQL解析、符合处理引擎等。
+ * 
+ */
+package com.pattern1._7interpreter;
